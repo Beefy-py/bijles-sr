b8454bb46377ea79bc2b752ff63292ff83b07332
Beefy-py <hoftkenny@gmail.com>
2023-09-23 00:06:08 -0300

    refactor: :wrench: added favicon config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,26 @@ export default function RootLayout({
     <html lang="en" style={{ scrollBehavior: "smooth", overflowX: "hidden" }}>
       <head>
         <ColorSchemeScript />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </head>
       <body className={`${inter.className}`}>
         <MantineProvider>
