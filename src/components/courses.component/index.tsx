c77328c5e0e367d8ffd570c04b6c023c21d00eca
Beefy-py <hoftkenny@gmail.com>
2023-09-27 21:10:47 -0300

    feat: :sparkles: added course detail page
    course detail page with locked video, pay with gwap

--- src/components/courses.component/index.tsx
+++ src/components/courses.component/index.tsx
@@ -22,40 +22,43 @@ const CoursesComponent = () => {
       <section id="courses" className="bg-white sm:py-16 lg:py-28">
         <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {coursesComponent.courses.map((course, index) => (
-            <Link
-              href="#"
+            <Card
               key={index + course.title}
-              className="flex flex-col lg:flex-row justify-between cursor-pointer"
+              radius="md"
+              className="w-full cursor-pointer"
             >
-              <Card radius="md" className="w-full">
-                {/* <CourseCardImageCourseComponent /> */}
-                <Title>{course.title}</Title>
+              {/* <CourseCardImageCourseComponent /> */}
+              <Title>{course.title}</Title>
 
-                <Group justify="space-between" mt="md" mb="xs">
-                  <Group>
-                    <div>Beoordeling:</div>
-                    <Rating fractions={4} value={course.rating} readOnly />
-                  </Group>
-                  <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">
-                    <IconSchool className="w-4 h-4 mr-1" />
-                    {course.durationInHours} uren
-                  </span>
+              <Group justify="space-between" mt="md" mb="xs">
+                <Group>
+                  <div>Beoordeling:</div>
+                  <Rating fractions={4} value={course.rating} readOnly />
                 </Group>
+                <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">
+                  <IconSchool className="w-4 h-4 mr-1" />
+                  {course.durationInHours} uren
+                </span>
+              </Group>
 
-                <Text size="sm" c="dimmed">
-                  {course.summary}
-                </Text>
+              <Text size="sm" c="dimmed">
+                {course.summary}
+              </Text>
 
-                <Group mt="md" gap="sm" grow>
-                  <Button variant="default" color={colors.tertiary.DEFAULT}>
-                    Bekijk Intro
-                  </Button>
-                  <Button variant="light" color={colors.tertiary.DEFAULT}>
-                    Meer Info
-                  </Button>
-                </Group>
-              </Card>
-            </Link>
+              <Group mt="md" gap="sm" grow>
+                <Button variant="default" color={colors.tertiary.DEFAULT}>
+                  Bekijk Intro
+                </Button>
+                <Button
+                  component={Link}
+                  href={`/courses/${course.title.toLowerCase()}`}
+                  variant="light"
+                  color={colors.tertiary.DEFAULT}
+                >
+                  Meer Info
+                </Button>
+              </Group>
+            </Card>
           ))}
         </div>
       </section>
