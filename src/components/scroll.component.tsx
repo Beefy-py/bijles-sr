cf1bc44ac0e7796f76b1d712a0e5a70662884d2a
Beefy-py <hoftkenny@gmail.com>
2023-10-14 10:20:14 -0300

    fix: :lipstick: scroll to top button had no bg color
    also added icon animation on hover

--- src/components/scroll.component.tsx
+++ src/components/scroll.component.tsx
@@ -4,9 +4,11 @@ import React from "react";
 import { IconArrowBigUp } from "@tabler/icons-react";
 import { useWindowScroll } from "@mantine/hooks";
 import { Affix, Button, Transition, rem } from "@mantine/core";
+import { theme } from "@tailwindConfig";
 
 const ScrollComponent = ({ children }: { children: React.ReactNode }) => {
   const [scroll, scrollTo] = useWindowScroll();
+  const { colors } = theme as any;
 
   return (
     <>
@@ -15,11 +17,16 @@ const ScrollComponent = ({ children }: { children: React.ReactNode }) => {
         <Transition transition="slide-up" mounted={scroll.y > 0}>
           {(transitionStyles) => (
             <Button
+              className="group transition"
               leftSection={
-                <IconArrowBigUp style={{ width: rem(16), height: rem(16) }} />
+                <IconArrowBigUp
+                  className="group-hover:animate-bounce"
+                  style={{ width: rem(16), height: rem(16) }}
+                />
               }
               style={transitionStyles}
               onClick={() => scrollTo({ y: 0 })}
+              color={colors.tertiary.DEFAULT}
             >
               Naar Boven
             </Button>
