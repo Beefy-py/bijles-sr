17e16f098d0863511fda73155f7de8bf27fb81d1
Beefy-py <hoftkenny@gmail.com>
2023-10-21 08:02:42 -0300

    feat: :sparkles: dropdown menu for register
    option to download gwap app on larger screens. register account with gwap

--- src/components/navbar.component/index.tsx
+++ src/components/navbar.component/index.tsx
@@ -1,10 +1,20 @@
 "use client";
 
-import { Button, Text } from "@mantine/core";
-import { IconBooks, IconLogin } from "@tabler/icons-react";
+import { Button, Divider, Menu, SimpleGrid, Text } from "@mantine/core";
+import {
+  IconBooks,
+  IconChevronCompactDown,
+  IconChevronDown,
+  IconExternalLink,
+  IconLogin,
+} from "@tabler/icons-react";
 import Link from "next/link";
 import React from "react";
-import { useDisclosure, useWindowScroll } from "@mantine/hooks";
+import {
+  useDisclosure,
+  useViewportSize,
+  useWindowScroll,
+} from "@mantine/hooks";
 import { Burger } from "@mantine/core";
 import ScrollProgressComponent from "./scrollprogress.navbar";
 import { navbarComponent } from "@/utils/content";
@@ -15,9 +25,11 @@ import { motion } from "framer-motion";
 const NavbarComponent = () => {
   const [scroll] = useWindowScroll();
   const [opened, { toggle }] = useDisclosure();
+  const { width } = useViewportSize();
   const scrolled = scroll.y > 40;
   // const textInvert = (670 < scroll.y && scroll.y < 930) || scroll.y < 100;
   const { colors } = theme as any;
+  const isSmallScreen = width < 700;
 
   return (
     <motion.header
@@ -37,6 +49,7 @@ const NavbarComponent = () => {
           <Link href="/" className="flex w-16 h-16 relative">
             <Image src={navbarComponent.logo} alt="Bijles SR Logo" fill />
           </Link>
+
           <div className="flex items-center lg:order-2 gap-2">
             <Button
               component={Link}
@@ -48,14 +61,118 @@ const NavbarComponent = () => {
             >
               {navbarComponent.ctaText1}
             </Button>
-            <Button
-              component={Link}
-              href="/hello"
-              color={colors.tertiary["300"]}
-              className="!text-tertiary-900"
+
+            <Menu
+              width={isSmallScreen ? 300 : 700}
+              shadow="md"
+              position="bottom-end"
+              withArrow
+              arrowPosition="center"
             >
-              {navbarComponent.ctaText2}
-            </Button>
+              <Menu.Target>
+                <Button
+                  color={colors.tertiary["300"]}
+                  variant="subtle"
+                  className="!text-tertiary-900"
+                  rightSection={<IconChevronDown size={16} />}
+                >
+                  {navbarComponent.ctaText2}
+                </Button>
+              </Menu.Target>
+
+              <Menu.Dropdown>
+                <SimpleGrid
+                  cols={isSmallScreen ? 1 : 2}
+                  spacing="xs"
+                  verticalSpacing="xs"
+                >
+                  {!isSmallScreen && (
+                    <Menu.Item
+                      className="relative h-auto z-10"
+                      style={{
+                        backgroundImage: "url(/images/gwap-app.jpg)",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                      }}
+                      styles={{
+                        item: {
+                          padding: "3rem",
+                        },
+                      }}
+                    >
+                      <div className="-z-10 bg-black/20 backdrop-blur-[2px] rounded absolute top-0 left-0 right-0 bottom-0"></div>
+                      <div className="overlay-content z-10">
+                        <p className="text-white font-semibold">
+                          Download de GWAP-app uit de Play Store of App Store en
+                          begin vandaag nog met betalen!
+                        </p>
+                        <Link
+                          href={"/"}
+                          className="cursor-pointer flex mt-3 py-2 bg-black text-white rounded items-center justify-center"
+                        >
+                          <div className="mr-3">
+                            <Image
+                              src={"/images/apple-store-logo.png"}
+                              width={25}
+                              height={25}
+                              alt="apple app store logo"
+                            />
+                          </div>
+                          <div>
+                            <div className="text-xs">Download via de</div>
+                            <div className="text-sm font-semibold -mt-1">
+                              App Store
+                            </div>
+                          </div>
+                        </Link>
+                        <Divider
+                          my="xs"
+                          color="white"
+                          label="Or"
+                          labelPosition="center"
+                        />
+                        <Link
+                          href={"/"}
+                          className="flex mt-3 bg-black py-2 text-white rounded items-center justify-center"
+                        >
+                          <div className="mr-3">
+                            <Image
+                              src={"/images/google-play-logo.png"}
+                              width={25}
+                              height={25}
+                              alt="google play store logo"
+                            />
+                          </div>
+                          <div>
+                            <div className="text-xs">Download via</div>
+                            <div className="text-sm font-semibold font-sans -mt-1">
+                              Google Play
+                            </div>
+                          </div>
+                        </Link>
+                      </div>
+                    </Menu.Item>
+                  )}
+                  <Menu.Item
+                    component={Link}
+                    href="/gwap-register"
+                    // target="_blank"
+                  >
+                    <div className="flex flex-col items-center">
+                      <h2 className="text-center text-tertiary-500 mb-3 text-2xl font-bold">
+                        Registreer met
+                      </h2>
+                      <Image
+                        src={"/images/gwap-logo-3.png"}
+                        alt="gwap logo"
+                        width={120}
+                        height={90}
+                      />
+                    </div>
+                  </Menu.Item>
+                </SimpleGrid>
+              </Menu.Dropdown>
+            </Menu>
             <Burger
               opened={opened}
               onClick={toggle}
