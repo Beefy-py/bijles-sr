f4ef2bb12e6eba345e6f7b15494bbd14e344cf65
Beefy-py <hoftkenny@gmail.com>
2023-11-11 12:30:26 -0300

    style: :lipstick: add some more paddingbottom for footer
    added padding bottom so icons dont stick to the bottom

--- src/components/footer.component/index.tsx
+++ src/components/footer.component/index.tsx
@@ -93,7 +93,7 @@ const FooterComponent = () => {
           </div>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="sm:flex sm:items-center sm:justify-between pb-6">
           <span className="text-sm text-gray-500 sm:text-center">
             © 2023{" "}
             <Link href="/" className="hover:underline">
