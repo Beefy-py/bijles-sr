9ca4af15bdf00bab7b6412fec43d24a47164ec78
Beefy-py <hoftkenny@gmail.com>
2023-11-18 20:19:11 -0300

    feat: :iphone: add more grid cols for xl screens

--- src/components/tutorials.component/index.tsx
+++ src/components/tutorials.component/index.tsx
@@ -16,7 +16,7 @@ const TutorialsComponent = () => {
         >
           Tutorials
         </h2>
-        <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {tutorialsComponent.tutorials.map((tutorial, index) => (
             <TutorialCardComponent
               key={index + tutorial.title}
